293445b5f076ce44041fcb343e6b5d60cff1167f
Lucas Mendes <lucasffm@gmail.com>
2023-07-05 16:41:09 -0300

    refactor: move passwordEncoder to SecurityConfiguration class

--- src/main/java/com/lucasffm/conduitapi/configs/SecurityConfiguration.java
+++ src/main/java/com/lucasffm/conduitapi/configs/SecurityConfiguration.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
@@ -19,4 +23,10 @@ public class SecurityConfiguration {
 
     return http.build();
   }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder(11);
+  }
+
 }
--- src/main/java/com/lucasffm/conduitapi/services/UserService.java
+++ src/main/java/com/lucasffm/conduitapi/services/UserService.java
@@ -3,7 +3,7 @@ package com.lucasffm.conduitapi.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lucasffm.conduitapi.configs.WebSecurityConfig;
+import com.lucasffm.conduitapi.configs.SecurityConfiguration;
 import com.lucasffm.conduitapi.models.UserModel;
 import com.lucasffm.conduitapi.repositories.UserRepository;
 
@@ -13,7 +13,7 @@ public class UserService {
   UserRepository userRepository;
 
   @Autowired
-  WebSecurityConfig webSecurityConfig;
+  SecurityConfiguration webSecurityConfig;
 
   public UserModel createUser(UserModel userModel) {
     userModel.setPassword(webSecurityConfig.passwordEncoder().encode(userModel.getPassword()));
